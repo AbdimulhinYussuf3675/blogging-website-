6654ef28b8650e2eca0c1be4a67ad1ce689a1e96
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-06-21 18:08:41 +0300

    added a db

--- config.py
+++ config.py
@@ -1,21 +1,19 @@
 import os
-import re 
 
 class Config:
-    SQLALCHEMY_TRACK_MODIFICATIONS=True
-    SECRET_KEY='SECRET_KEY'
+    SECRET_KEY ='SECRET_KEY'
+    UPLOADED_PHOTOS_DEST ='app/static/images'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SENDER_EMAIL = 'abdimulhin.yussuf@student.moringaschool.com'
     
-
-# class ProdConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
+    
+    @staticmethod
+    def init_app(app):
+        pass
+    
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
     
@@ -29,9 +27,7 @@ class TestConfig(Config):
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/adam'
     DEBUG = True
-
-
-
+    
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
