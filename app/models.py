cda3aeba14a2a5405e1b182ced8151311c2eeb4e
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-06-22 14:03:09 +0300

    deployed to heroku

--- app/models.py
+++ app/models.py
@@ -16,7 +16,6 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
-    pitches = db.relationship('Pitch', backref='user', lazy="dynamic")
     comments = db.relationship("Comment", backref="user", lazy="dynamic")
     
     @property
@@ -33,29 +32,29 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.username}'
     
-class Pitch(db.Model):
-    __tablename__ = 'pitches'
-    id = db.Column(db.Integer, primary_key=True)
-    title= db.Column(db.String(300), index=True)
-    content = db.Column(db.String(300), index=True)
-    category = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    comments = db.relationship('Comment', backref='pitch', lazy="dynamic")
-    date = db.Column(db.String)
-    time = db.Column(db.String)
+# class Pitch(db.Model):
+#     __tablename__ = 'pitches'
+#     id = db.Column(db.Integer, primary_key=True)
+#     title= db.Column(db.String(300), index=True)
+#     content = db.Column(db.String(300), index=True)
+#     category = db.Column(db.String)
+#     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+#     comments = db.relationship('Comment', backref='pitch', lazy="dynamic")
+#     date = db.Column(db.String)
+#     time = db.Column(db.String)
     
-    def save_pitch(self, pitch):
-        ''' Save the pitches '''
-        db.session.add(pitch)
-        db.session.commit()
+#     def save_pitch(self, pitch):
+#         ''' Save the pitches '''
+#         db.session.add(pitch)
+#         db.session.commit()
 
-    @classmethod
-    def get_pitches(id):
-        pitches = Pitch.query.filter_by(category = id).all()
-        return pitches
+#     @classmethod
+#     def get_pitches(id):
+#         pitches = Pitch.query.filter_by(category = id).all()
+#         return pitches
     
-    def __repr__(self):
-        return f"Pitch('{self.id}', '{self.time}')"
+#     def __repr__(self):
+#         return f"Pitch('{self.id}', '{self.time}')"
     
 class Comment(db.Model):
     __tablename__ = 'comments'
